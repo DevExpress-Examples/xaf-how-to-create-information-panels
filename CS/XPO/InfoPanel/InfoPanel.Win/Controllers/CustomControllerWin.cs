f5f0d73c1c4d43e5eeaa2078c7f9603216e6f94f
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-28 19:24:03 +0400

    win finisр

--- CS/XPO/InfoPanel/InfoPanel.Win/Controllers/CustomControllerWin.cs
+++ CS/XPO/InfoPanel/InfoPanel.Win/Controllers/CustomControllerWin.cs
@@ -22,76 +22,32 @@ using dxTestSolution.Module.BusinessObjects;
 using DevExpress.ExpressApp.Win;
 
 namespace dxTestSolution.Module.Controllers {
-
-
     public interface IInfoPanelTemplateWin : IFrameTemplate {
         SidePanel SidePanel {
             get;
         }
     }
 
-    public class InfoPanelViewControllerWin : CustomizeTemplateViewControllerBase<IInfoPanelTemplateWin> {
-        LabelControl label;
-        protected override void AddControlsToTemplateCore(IInfoPanelTemplateWin template) {
-            if (label == null) {
-                label = new LabelControl();
-            }
-            template.SidePanel.Controls.Add(label);
-        }
-        protected override void RemoveControlsFromTemplateCore(IInfoPanelTemplateWin template) {
-            template.SidePanel.Controls.Remove(label);
-            label = null;
-        }
-        protected override void UpdateControls(DevExpress.ExpressApp.View view) {
-            UpdateControls();
-        }
-        protected override void UpdateControls(object currentObject) {
-            UpdateControls();
-        }
-
-              void UpdateControls() {
-            label.Text = "The current View is " + View.Caption;
-            if (View.CurrentObject != null) {
-                label.Text += System.Environment.NewLine +
-                    "The current object is " + View.CurrentObject.ToString();
-            }
-        }
-    }
-
-    public class CustomWinController : ObjectViewController<ListView,Contact> {
-    //public class CustomWinController : ViewController {
-        public CustomWinController() {
-            var myAction1 = new SimpleAction(this, "MyWinAction1", null);
-            myAction1.Execute += MyAction1_Execute;
-            
-        }
-
-        private void MyAction1_Execute(object sender, SimpleActionExecuteEventArgs e) {
-            //var os = Application.CreateObjectSpace(typeof(MyNonPersistentClass));
-            //var obj = os.CreateObject<MyNonPersistentClass>();
-            //var detailView = Application.CreateDetailView(os, obj);
-
-            var tst = this.Frame as IInfoPanelTemplateWin;
-            var t2=((WinWindow)this.Application.MainWindow).Form as IInfoPanelTemplateWin;
-            var label = new Label();
-            label.Width= 1000;
-            label.Height = 200;
-            label.Text = "The current View is " + View.Caption;
-            t2.SidePanel.Controls.Add(label);
-        }
-
+    public class CustomWinController : ObjectViewController<ListView, Contact> {
         protected override void OnActivated() {
             base.OnActivated();
-            var cnt = Frame.GetController<NewObjectViewController>();
-            if(cnt != null) {
+            this.View.CurrentObjectChanged += View_CurrentObjectChanged;
+        }
 
+        private void View_CurrentObjectChanged(object sender, EventArgs e) {
+            var infoPanelTemplate = ((WinWindow)this.Application.MainWindow).Form as IInfoPanelTemplateWin;
+            Label label;
+            if (infoPanelTemplate.SidePanel.Controls.Count == 0) {
+                label = new Label();
+                label.Width = 1000;
+                label.Height = 200;
+                infoPanelTemplate.SidePanel.Controls.Add(label);
+            } else {
+                label = (Label)infoPanelTemplate.SidePanel.Controls[0];
             }
+            label.Text = "The current Object is " + ((Contact)View.CurrentObject).FirstName;
         }
-        protected override void OnViewControlsCreated() {
-            base.OnViewControlsCreated();
-        }
-        protected override void OnDeactivated() {
-            base.OnDeactivated();
-        }
+
+
     }
 }
